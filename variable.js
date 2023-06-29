15af811cd7b92a7346712091d495730c09e875b7
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-06-29 21:29:09 +0500

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,24 @@
+// console.log("HI");
+// document.getElementById("heading").innerHTML = "Hi Buddy!!!";
+// document.write("Hi");
+
+// Variable-> container to store data
+
+/*
+Multi Line Comment
+*/
+//Single Comment
+
+// Declare
+// var firstName;
+// Initialize
+// firstName = 5;
+
+// Declare + Initialization
+// var firstName = 5;
+// firstName = 8;
+// let firstName = 10;
+// let firstName = "Farzeen Ali";
+const firstName = 4;
+// firstName = 6; ->error due to your variable is constant
+console.log(firstName);
