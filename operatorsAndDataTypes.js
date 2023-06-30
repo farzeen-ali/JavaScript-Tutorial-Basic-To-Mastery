593dba5efbd7297df1ae51bcec2466d2dc862c0d
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-07-01 00:19:27 +0500

    Add files via upload

---
+++ operatorsAndDataTypes.js
@@ -0,0 +1,77 @@
+/*
+Operations-> process of operator
+operand -> jis pr operation perform horha hi
+operator -> type of operation
+a+b;
+*/
+// let x = 5; // = -> assignment operator
+// let y = 10;
+// let z;
+
+// z = x+y;
+// z = x-y;
+// z = y/x;
+// z = x*y;
+// z = y%x;   -> % modulus operator
+
+// Power
+// z = x ** 3;
+// z = x ** 2;
+// console.log(z);
+
+// y = "5"; //->string
+// y = "Hello";
+
+// + -> concatenation->combine
+// console.log(x+" "+y);
+// console.log(y+" "+x);
+
+// Increment And Decrement Operator
+//x++;  //x++   ->  x = x+1
+//x--;  //x--   ->  x = x-1
+
+//Shorthand Properties
+// x = x+y; -> x+=y
+// x = x-y; -> x-=y
+// x = x*y;-> x*=y
+// x = x/y;-> x/=y
+// x = x%y;-> x%=y
+
+// z = (x + 5) /5;  //BDMAS -> Bracket Division Mul Add Sub
+// console.log(z);
+
+
+/***********************Data Type************************ */
+// let x = "Hello";
+// let x = 'Hel"lo';
+// let x = "Hel'lo";
+// let x = "Hel\"lo";
+// let x = 'Hel\'lo';
+// x = 5.35; -> float
+// let x = 3; -> integer
+
+// boolean -> true or false
+// let x;
+// // x = 5>8;
+// x = 5<8;
+// undefined -> variable not initialized
+// let x;
+
+// let x = typeof 10;
+// let x = typeof 'Hello';
+// let x = 3;
+// console.log(x);
+// console.log(typeof x);
+
+
+let x = 'Hello hi Hello';
+let y = "World";
+let z = x+y;
+console.log(`Hi ${x}`); //template literal-> use tild operator `
+
+
+
+
+
+
+
