128c6f32d71a85d767bb2f221781284963ef9e96
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-07-02 17:55:12 +0500

    Add files via upload

---
+++ comparisonAndLogicalOperator.js
@@ -0,0 +1,58 @@
+/*
+Comparison Operator: 
+Compare
+
+1) == -> equal to (nos equal) 
+e.g: 
+ 3 == 4; -> false
+ 2 == "2" -> true
+
+2) === -> equal to (checks No and Data type)
+ 3 === 4; -> false
+ 3 === "3" -> false
+ 3 === 3 ->true
+ "3" === "3" ->true
+
+3) != -> not equal
+    4!=5 -> true
+    5!=5 -> false
+
+4) !== ->not equal value not equal type
+    5!==5 -> false
+    5!=="5"-> true
+    5!==8-> false
+
+5) greater than >
+3>2->true
+2>3->false
+
+6) less than <
+2<3 -> true
+3<2 ->false
+
+7)greater than equal to >=
+3>=3-> true
+2>=3 -> false
+
+8) less than equal to <=
+2<=3 ->true
+2<=2 -> true
+3<=2 -> false
+
+Logical Operators:
+1) And && -> all values must be true for true ans
+ 
+(2<4 && 2<8) -> True
+(2<4 && 3<2) -> False
+
+2) OR || -> min 1 true -> true 
+
+(2<4 && 2<8) -> True
+(2<4 && 3<2) -> True
+(3<2 && 4<3) -> false
+
+3) ! not operator(opposite result) (true)->false  (false)->true
+!(6==5)  -> true
+!(5==5) -> false
+
+*/
