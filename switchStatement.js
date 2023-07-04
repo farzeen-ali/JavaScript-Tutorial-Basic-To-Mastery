c1da15a87a992e9ba413661010bb2249e49effc1
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-07-04 16:49:26 +0500

    Add files via upload

---
+++ switchStatement.js
@@ -0,0 +1,93 @@
+// **********************Switch Statement****************************
+/*
+A block of code depending on different cases. (Conditional Statement)
+Syntax:
+switch(expression)  age =20
+{
+    case x:     10
+        code
+        break;
+        
+    case y:    20
+        code
+        break;
+        . 
+        . 
+        . 
+        default:
+            code
+}
+*/
+
+// let day = 2;
+
+// switch(day)
+// {
+//     case 1: 
+//     document.write("Sunday");
+//     break;
+//     case 2: 
+//     document.write("Monday");
+//     break;
+//     case 3: 
+//     document.write("Tuesday");
+//     break;
+//     case 4: 
+//     document.write("Wednesday");
+//     break;
+//     case 5: 
+//     document.write("Thursday");
+//     break;
+//     case 6: 
+//     document.write("Friday");
+//     break;
+//     case 7: 
+//     document.write("Saturday");
+//     break;
+//     default:
+//     document.write("Invalid Day");
+// }
+
+
+// let variable = 2;
+
+// switch(variable)
+// {
+//     case 1: 
+//         document.write("True");
+//         break;
+//     case 'y':
+//         document.write("True");
+//         break;
+//     case "yes":
+//         document.write("True");
+//         break;
+//     case 0:
+//         document.write("False");
+//         break;
+//     case 'n':
+//         document.write("False");
+//         break;
+//     case "no":
+//         document.write("False");
+//         break;
+//     default:
+//         document.write("Invalid");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
