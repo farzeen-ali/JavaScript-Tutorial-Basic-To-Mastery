d0d949539198e0b641b1cfcdbc2c643727d89138
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-07-07 23:11:56 +0500

    Add files via upload

---
+++ break,continue,nestedLoops.js
@@ -0,0 +1,90 @@
+/*
+Break: the control exits from the loop
+Continue: skip particular iteration of the loop but control remain within the loop
+Nested Loop: a loop presented inside the another loop
+*/
+
+// for(let i = 0; i<=10; i++)
+// {
+//     if(i==5)
+//     {
+//         break;
+//     }
+//     document.write("The Value is: " + i + "<br>");
+// }
+
+// for(let i = 0; i<=10; i++)
+// {
+//     if(i==5)
+//     {
+//         continue;
+//     }
+//     document.write("The Value is: " + i + "<br>");
+// }
+
+
+// for(let i =0; i<=5; i++)
+// {
+//     document.write("The Value of I is: " + i + "<br>");
+
+//     for(let j=0; j<2; j++)
+//     {
+//         document.write("The Value of J is: " + j + "<br>");
+//     }
+
+// }
+
+// bahar: for(let i =0; i<=5; i++)
+// {
+//     document.write("The Value of I is: " + i + "<br>");
+
+//     for(let j=0; j<2; j++)
+//     {
+//         if(i == 2)
+//         {
+//             break bahar;
+//         }
+
+//         document.write("The Value of J is: " + j + "<br>");
+//     }
+
+// }
+
+
+// let count = 0;
+// while(count<10)
+// {
+//     count++;
+//     if(count%2 == 0)
+//     {
+//         continue;
+//     }
+//     document.write("The Value IS: " + count + "<br>");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
