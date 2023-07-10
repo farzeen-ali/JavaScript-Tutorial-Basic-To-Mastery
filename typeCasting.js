ad031f367194069e14f87bc0a06d4859dc6b5304
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-07-10 22:21:46 +0500

    Add files via upload

---
+++ typeCasting.js
@@ -0,0 +1,85 @@
+// *****************Type Casting***************
+
+// let number = true;
+// document.writeln(number);
+// document.writeln(typeof number);
+
+// let number = 4 + null;
+// document.writeln(number);
+// document.writeln(typeof number);
+
+// let number = "3" + 2;
+// document.writeln(number);
+// document.writeln(typeof number);
+
+// let number = "3" - 2;
+// document.writeln(number);
+// document.writeln(typeof number);
+
+// let number = "3" * "yes";
+// document.writeln(number);
+// document.writeln(typeof number);
+
+
+/*
+1) String()
+2) Number()
+3) Boolean()
+
+*/
+
+// let oldType = 2;
+// document.writeln(typeof oldType);
+
+// let newType = String(oldType);
+// document.writeln(typeof newType);
+
+// let oldType = "32";
+// document.writeln(typeof oldType);
+
+// let newType = Number(oldType);
+// document.writeln(typeof newType);
+
+// let oldType = "32";
+// document.writeln(typeof oldType);
+
+// let newType = Boolean(oldType);
+// document.writeln(typeof newType);
+
+// let oldType = false;
+// document.writeln(typeof oldType);
+
+// let newType = Number(oldType);
+// document.writeln(newType);
+// document.writeln(typeof newType);
+
+// let oldType = false;
+// document.writeln(typeof oldType);
+
+// let newType = String(oldType);
+// document.writeln(typeof newType);
+
+// let oldType = 0;
+// document.writeln(typeof oldType);
+
+// let newType = Boolean(oldType);
+// document.writeln(newType);
+// document.writeln(typeof newType);
+
+// let oldType = "";
+// document.writeln(typeof oldType);
+
+// let newType = Number(oldType);
+// document.writeln(newType);
+// document.writeln(typeof newType);
+
+
+
+
+
+
+
+
+
+
+
