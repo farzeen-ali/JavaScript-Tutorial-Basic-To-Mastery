10b6f46fb472e26d8732e074039182d31cdeb2cc
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-07-12 22:45:19 +0500

    Add files via upload

---
+++ stringManipluation.js
@@ -0,0 +1,62 @@
+// String Manipulation
+// let message = "This is \n good";
+// let message = "This is \t good";
+// let message = "This is\" good";
+// let message = 'This is\' good';
+// let message = 'This is good';
+// console.log(message.length);
+// let message = 'This is good';
+// console.log(message[8]);
+// let firstName = "Farzeen";
+// let lastName = "Ali";
+// console.log(firstName + " " + lastName);
+// let firstName = "Farzeen";
+
+// if(firstName == "Farzeen")
+// {
+//     console.log("Equal");
+// }
+// else
+// {
+//     console.log("Not Equal");
+// }
+
+// let firstName = "Farzeen";
+// let lastName = "Ali";
+// let result = firstName.concat(" ",lastName);
+
+let message = 'This is good.';
+// let result = message.substr(5,2)
+// let result = message.substring(5,12)
+// let result = message.indexOf('good')
+// let result = message.indexOf('is',3);
+// let result = message.lastIndexOf('is');
+
+// let result = message.trim();
+// let result = message.trimStart();
+// let result = message.trimEnd();
+
+// let result = message.toUpperCase();
+// let result = message.toLowerCase();
+
+// let result = message.replace('This', 'Farzeen');
+let result = message.includes("Good");
+
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
