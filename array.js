df6e97c131e22e8b6e69cae5ff13fec7f166b60c
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-07-13 17:05:57 +0500

    Add files via upload

---
+++ array.js
@@ -0,0 +1,25 @@
+/* ***********Arrays************
+    storing multiple elements in a single variable
+*/
+// let students = "Farzeen";
+// let students1 = "Ali";
+// let students2 = "Ahsan";
+
+// let students = ["Farzeen","Ali","Ahsan","Ayesha","Iqra","Huzaifa"];
+
+// Second Method
+// let students = new Array("Farzeen","Ali","Ahsan","Ayesha","Iqra","Huzaifa");
+
+// console.log(students[4]);
+
+// let info = [1, "Ali", 60.60];
+// console.log(info);
+// console.log(info[2]);
+
+// let subjects = ["Maths","Science","English","Computer"];
+// for(let i = 0; i<4; i++)
+// {
+//     console.log(subjects[i]);
+//     console.log("\n");
+// }
+
