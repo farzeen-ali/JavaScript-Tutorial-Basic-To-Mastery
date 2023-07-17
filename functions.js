b681cfea10aba8efed3d5b97d7145c4aad0a6bad
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-07-17 16:16:14 +0500

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,29 @@
+// Function Define
+// function greet()
+// {
+//     console.log("Hello I'm Farzeen Ali");
+// }
+
+// greet();
+// greet();
+
+//Add Two Numbers
+// function add(a,b)
+// {
+//     console.log(a+b);
+// }
+
+// calling functions
+// add(5,6);
+// add(2,6);
+// add(3,6);
+// add(4,6);
+
+function greet(name)
+{
+    console.log(`Hello ${name}!`);
+}
+let name = prompt("Enter Your Name: ");
+
+greet(name);
+
