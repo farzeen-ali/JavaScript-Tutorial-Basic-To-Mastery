1294f8868ee97d21798f657a59367c135085cc6e
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-07-24 16:59:39 +0500

    Add files via upload

---
+++ return(Functions).js
@@ -0,0 +1,33 @@
+// function fullName(a,b)
+// {
+//     // let answer = (a+" " +b);
+//     // return answer
+//     return 0;
+//     return (a+" " +b);
+// }
+
+// let result = fullName("Farzeen","Ali");
+// console.log(result);
+
+
+function checkNumber(a)
+{
+    if(a>0)
+    {
+        return "Positive";
+    }
+    else if(a<0)
+    {
+        return "Negative";
+    }
+    else{
+        return "Zero";
+    }
+}
+
+let result = checkNumber(-2);
+console.log(result);
+
+
+
+
