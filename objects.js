9328d57f7c54e13e9ef1365ce6ca55b021172b9a
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-08-03 23:30:08 +0500

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,11 @@
+/**************** Objects In JavaScript **************************** 
+An Entity having state and behavior.(properties and method)
+e.g:
+car,pen,laptop,employee
+
+How to model a problem in Objects:
+Noun--> Object ----> Employee
+Adjective----> Properties(Attributes) ---> empName, empAge, empSalary....
+Verb----> Method(Behavior or Action) ----> sayHello()....
+*/
+
