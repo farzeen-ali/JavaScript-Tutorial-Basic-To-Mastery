bf12e720fd7e4f0aa016badfa1b19c432bf4ecda
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-08-07 23:34:43 +0500

    Add files via upload

---
+++ mathObject.js
@@ -0,0 +1,17 @@
+/****************** Math Object In JavaScript    **********************/
+// Allows you to perform mathematical task on numbers
+
+let result;
+
+// result = Math.abs(-22); //negative to positive
+// result = Math.round(2.6); //round up
+// result = Math.round(2.2); //round down
+// result = Math.PI; //for pi value
+// result = Math.ceil(7.2); //round up
+// result = Math.floor(7.8); //round down
+// result = Math.trunc(77.8); //truncate the decimal value
+// result = Math.pow(2,4); //first argument -> base , second argument-> power
+// result = Math.sqrt(49); //square root
+// result = Math.min(1,100,200,-3,0.5);//find the minimum value
+result = Math.max(1,100,200,-3,0.5);//find the minimum value
+console.log(result);
