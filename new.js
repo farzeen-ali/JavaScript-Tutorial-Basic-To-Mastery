20b9241004241450aa121b276d7609c153f9db6f
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-08-09 22:59:09 +0500

    Add files via upload

---
+++ new.js
@@ -0,0 +1,20 @@
+/**************** New Keyword In JavaScript*****************/
+// used to create an instance of an object that has a constructor function.
+
+// let employee = {
+//     name : "Farzeen",
+//     salary : 50000
+// }
+
+
+//object creation using new keyword
+
+let employee = new Object();
+
+employee.name = "Farzeen Ali";
+employee.salary = 60000;
+
+console.log(employee);
+
+// console.log(employee.name);
+// console.log(employee.salary);
