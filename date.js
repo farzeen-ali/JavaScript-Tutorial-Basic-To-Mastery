bee2a2f80e316557fe8784200dc6df0bfe7faf81
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-08-11 23:27:14 +0500

    Add files via upload

---
+++ date.js
@@ -0,0 +1,4 @@
+/****************** Date Object In JavaScript ***************************/
+//built-in object that stores the date and time.
+//provides a number of built-in methods for formatting and managing the date
+
