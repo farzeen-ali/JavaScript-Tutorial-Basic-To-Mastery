b59cbfa0da00136352a012d572460d60b95cb7a1
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-09-10 22:19:19 +0500

    Add files via upload

---
+++ getElementsByClassName.js
@@ -0,0 +1,12 @@
+// let element = document.getElementsByClassName("programming");
+// console.log(element.length);
+// element.innerHTML = "Hi";---->not working
+// for(let i = 0; i < element.length; i++){
+//     element[i].innerHTML = "Jalwa Hai Hamara";
+// }
+
+
+// let element = document.getElementsByClassName("maths");
+// for(let i = 0; i < element.length; i++){
+//     element[i].innerHTML = "Jalwa Hai Hamara";
+// }
