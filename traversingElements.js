93173279be1cef5fed7146f01809cdd1ed95b64d
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-09-16 23:37:00 +0500

    Add files via upload

---
+++ traversingElements.js
@@ -0,0 +1,31 @@
+// For Parent Elements
+// let element = document.getElementById('me');
+// let element = document.getElementById('me').parentElement;
+// console.log(element);
+// element.innerHTML = "Farzeen";
+// let parentElement = element.parentElement;
+// console.log(parentElement);
+
+//For First and Last Child
+// let element = document.getElementById('hi')
+// console.log(element)
+// let childElement = element.firstElementChild;
+// let childElement = element.lastElementChild;
+// console.log(childElement)
+
+//For Children Elements
+// let element = document.getElementById('hi')
+// let children = element.children;
+// console.log(children);
+
+//For Siblings
+// let element = document.getElementById('me');
+// let previousElement = element.previousElementSibling;
+// console.log(previousElement);
+// let nextElement = element.nextElementSibling;
+// console.log(nextElement);
+
+//By using class name
+let element = document.querySelector('li.you')
+let parentElement = element.parentElement;
+console.log(parentElement);
