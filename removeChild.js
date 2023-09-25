558fd925743279d81935cf222f97cad08d743867
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-09-25 22:40:18 +0500

    Add files via upload

---
+++ removeChild.js
@@ -0,0 +1,6 @@
+let parent = document.getElementById("items");
+// let removeElement = parent.firstElementChild.nextElementSibling;
+// console.log(removeElement);
+// parent.removeChild(removeElement);
+
+document.body.removeChild(parent);
