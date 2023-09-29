54a5251a3b6f33aec96ce1f6d859c3e1b41a2207
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-09-29 21:59:31 +0500

    Add files via upload

---
+++ insertAdjacentHTML.js
@@ -0,0 +1,8 @@
+let selectedElement = document.getElementById("main");
+
+let newElement = "<h1>Farzeen Ali</h1>";
+
+// selectedElement.insertAdjacentHTML("beforebegin", newElement);
+// selectedElement.insertAdjacentHTML("afterbegin", newElement);
+// selectedElement.insertAdjacentHTML("beforeend", newElement);
+selectedElement.insertAdjacentHTML("afterend", newElement);
