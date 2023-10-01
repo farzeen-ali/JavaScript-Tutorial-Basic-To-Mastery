a7141204ca58ffee26e2cb5f31a86277c3ffffc9
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-10-01 22:53:08 +0500

    Add files via upload

---
+++ inlineStyle.js
@@ -0,0 +1,12 @@
+let selectedElement = document.getElementById("main");
+
+//First Method
+// selectedElement.style.cssText = "background-color: aqua;";
+selectedElement.style.cssText += "background-color: purple;"
+
+//Second Method
+// selectedElement.setAttribute("style","color: white; background-color: purple;")
+
+//Third Method
+// selectedElement.style.color = "aqua";
+// selectedElement.style.backgroundColor = "red";
