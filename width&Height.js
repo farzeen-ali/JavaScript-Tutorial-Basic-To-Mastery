cc7ec3de25477fde3a6fd1c7d39d8d8228d27d1b
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-10-03 22:26:08 +0500

    Add files via upload

---
+++ width&Height.js
@@ -0,0 +1,9 @@
+let element = document.getElementById("box");
+
+// let width = element.offsetWidth;
+let width = element.clientWidth;
+console.log(width);
+
+// let height = element.offsetHeight;
+let height = element.clientHeight;
+console.log(height);
