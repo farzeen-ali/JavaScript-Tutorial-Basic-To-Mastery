9493f3578f42c5c65478ecc6fdc86433f5fa46cf
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-10-04 22:17:05 +0500

    Add files via upload

---
+++ cssClasses.js
@@ -0,0 +1,39 @@
+let element = document.getElementById("box");
+
+//find className on an element
+// console.log(element.className);
+
+//how to add className using dom manipulation
+// element.className += " color";
+
+//how to check className list on an element
+// console.log(element.classList);
+
+// for(let style of element.classList){
+//     console.log(style);
+// }
+
+//how to add a className 
+// element.classList.add("color");
+
+//how to remove a className 
+// element.classList.remove("square");
+
+//how to replace a className
+// element.classList.replace("square","color");
+
+//how to check existence of a className
+// let result = element.classList.contains("square");
+// console.log(result);
+
+//how to check existence of a className
+element.classList.toggle("color");
+
+
+
+
+
+
+
+
+
