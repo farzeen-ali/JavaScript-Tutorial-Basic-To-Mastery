3d384b93fa337dcdf23c3368aa20e361658856b6
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-10-06 22:45:46 +0500

    cssClasses.js

--- cssClasses.js
+++ cssClasses.js
@@ -1,39 +1,30 @@
 let element = document.getElementById("box");
 
-//find className on an element
+//find className of an element
 // console.log(element.className);
 
-//how to add className using dom manipulation
+//how to add class using className Property
 // element.className += " color";
 
-//how to check className list on an element
+//how to check className list of an element
 // console.log(element.classList);
 
 // for(let style of element.classList){
 //     console.log(style);
 // }
 
-//how to add a className 
+//how to add a className
 // element.classList.add("color");
 
-//how to remove a className 
+//how to remove a className
 // element.classList.remove("square");
 
 //how to replace a className
 // element.classList.replace("square","color");
 
-//how to check existence of a className
-// let result = element.classList.contains("square");
+//how to check existence of a class
+// let result = element.classList.contains("color");
 // console.log(result);
 
-//how to check existence of a className
+//toggle class add/remove
 element.classList.toggle("color");
-
-
-
-
-
-
-
-
-
