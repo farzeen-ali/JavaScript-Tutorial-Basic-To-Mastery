a6506d48a628b9ef6681e1742b73d602c73cb5ab
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-10-11 22:40:50 +0500

    Add files via upload

---
+++ pageLoadEvent.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', function(){
+    let heading = document.getElementById("main");
+    heading.textContent = "DOM Loaded!"
+})
+
+
+window.addEventListener('load', function(){
+    alert("Page Fully Loaded!")
+})
