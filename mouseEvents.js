e2dc1ee82c180797324a82663627d36f40b4c70f
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-10-12 22:45:30 +0500

    Add files via upload

---
+++ mouseEvents.js
@@ -0,0 +1,23 @@
+let elementBtn = document.getElementById("btn");
+
+//Click Event Listener
+// elementBtn.addEventListener('click',function(){
+//     console.log("Click Done!!!")
+// })
+
+//contextMenu Event Listener
+// elementBtn.addEventListener('contextmenu',function(){
+//     console.log("Right Click Done!!!")
+// })
+// //doubleClick Event Listener
+// elementBtn.addEventListener('dblclick',function(){
+//     console.log("Double Click Done!!!")
+// })
+//mouseOver Event Listener
+// elementBtn.addEventListener('mouseover',function(){
+//     console.log("Mouse Over Done!!!")
+// })
+//mouseOut Event Listener
+elementBtn.addEventListener('mouseout',function(){
+    console.log("Mouse Out Done!!!")
+})
