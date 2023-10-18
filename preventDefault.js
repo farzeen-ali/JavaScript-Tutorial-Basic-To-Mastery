a1af5279a45df5de11b595e915b1916cad1d6b20
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-10-18 22:44:00 +0500

    Add files via upload

---
+++ preventDefault.js
@@ -0,0 +1,12 @@
+// let element = document.getElementById("anchor");
+
+// element.addEventListener("click", function(event){
+//     console.log("Link Clicked")
+//     event.preventDefault();
+// })
+
+let formElement = document.getElementById("myForm");
+
+formElement.addEventListener("submit", function(e){
+    e.preventDefault();
+})
