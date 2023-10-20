ab8cbc6b897fc866467732c95ff27fe06e7f8a3e
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-10-20 23:49:55 +0500

    Add files via upload

---
+++ BOM.js
@@ -0,0 +1,30 @@
+/* ******************* BOM In JavaScript    *************************
+
+used to interact with browser
+
+**** Window Object represents a window in browser (Default Object of Browser) *****
+
+global javaScript object, functions and variables with var keyword automatically become members of the window object
+
+global variables are properties of the window object 
+global functions are methods of the window object 
+global objects are nested-objects of the window object 
+
+*/
+
+// var age = 18;
+// let age = 18;
+// console.log(window.age);
+
+function message(){
+    console.log("Hello");
+}
+
+// message();
+// window.message();
+
+// console.log(window);
+
+// alert("Farzeen");
+
+window.alert("HI Window");
