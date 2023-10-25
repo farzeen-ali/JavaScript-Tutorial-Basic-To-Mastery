2f7b1409173b525994ec47111c377ea8b9a541f1
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-10-25 23:40:17 +0500

    Add files via upload

---
+++ navigatorObject.js
@@ -0,0 +1,7 @@
+// console.log(navigator.appName);
+// console.log(navigator.appVersion);
+// console.log(navigator.appCodeName);
+// console.log(navigator.cookieEnabled);
+// console.log(navigator.userAgent);
+// console.log(navigator.platform);
+console.log(navigator.javaEnabled());
