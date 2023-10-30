ccb49537cef2f861a8e4ebf5da925c6ea2f7b469
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-10-30 21:58:36 +0500

    Add files via upload

---
+++ let&const.js
@@ -0,0 +1,23 @@
+// var age = 18;
+// let age = 18;
+// console.log(window.age)
+// console.log(age);
+
+//re-declaration
+// var a = 10;
+// var a;
+
+// let a = 10;
+// let a;  //error
+
+// console.log(a);
+
+//const keyword
+// var b = 2;
+// b = 3;
+// let b = 2;
+// b = 3;
+
+const b = 4;
+b = 2;
+console.log(b);
