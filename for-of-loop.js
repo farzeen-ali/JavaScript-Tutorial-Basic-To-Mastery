a6381809c32921de5206c3fe6cbb5220d8cafe3b
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-11-03 22:31:16 +0500

    Add files via upload

---
+++ for-of-loop.js
@@ -0,0 +1,18 @@
+let arr = [1,2,3,4,5];
+
+// for(let i = 0; i < arr.length; i++)
+// {
+//     console.log(arr[i]);
+// }
+
+//for of loop
+
+// for(let a of arr){
+//     console.log(a)
+// }
+
+let str = "Farzeen Ali";
+
+for(let s of str){
+    console.log(s);
+}
