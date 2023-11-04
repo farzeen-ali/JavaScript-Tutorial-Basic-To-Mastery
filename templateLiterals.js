58365e7b93cee6afd6e945a777646506af5d3716
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-11-04 23:06:19 +0500

    Add files via upload

---
+++ templateLiterals.js
@@ -0,0 +1,17 @@
+// let str = "Farzeen \n Ali";
+// let str = `
+// Farzeen 
+
+
+
+// Ali`;
+
+// console.log(str)
+
+
+let name = "Farzeen Ali";
+
+let age = 20;
+
+console.log("My name is " + name + " and I'm a Youtuber")
+console.log(`My name is ${name} and I'm a Youtuber and my age is ${age*2}`);
