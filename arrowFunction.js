4b09f955bbdbd2a6353a63028e65bfbeecc2e739
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-11-10 23:36:35 +0500

    Add files via upload

---
+++ arrowFunction.js
@@ -0,0 +1,60 @@
+//Normal Function
+// function sum(a,b){
+//     console.log(a+b);
+// }
+// sum(2,3);
+
+//Arrow Function
+// let sum = (a,b) => {
+//     console.log(a+b);
+// }
+// sum(2,3);
+
+//Another Short From
+// let sum = (a,b) => {
+//     return a+b;
+// }
+
+// console.log(sum(2,4));
+
+// let sum = (a,b) => a+b;
+
+// console.log(sum(2,4));
+
+//When one parameter
+
+// function double(a){
+//     return a*a;
+// }
+
+// console.log(double(3));
+
+//Arrow Function
+// let double = a => a*a;
+// console.log(double(4));
+
+//when no parameter
+// function greetings(){
+//     console.log("Hi")
+// }
+// greetings();
+
+//Arrow Function
+// let greetings = () =>  console.log("Good Morning");
+// greetings();
+
+//Anonymous Function
+// let a = function (){
+//     console.log("Welcome");
+// }
+// a();
+
+//Arrow Function
+// let a = () => console.log("Good Night");
+// a();
+
+// document.addEventListener('click', function(){
+//     console.log("Farzeen")
+// })
+//Arrow Function
+document.addEventListener('click', () => console.log("Farzeen"))
