c382809acecda4389f85a25c8f4f4cc11b3a25a8
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-12-02 22:40:50 +0500

    Add files via upload

---
+++ sessionStorage.js
@@ -0,0 +1,5 @@
+sessionStorage.setItem("name", "Farzeen");
+sessionStorage.setItem("age", "22");
+console.log(sessionStorage.getItem('name'));
+sessionStorage.removeItem('age');
+sessionStorage.clear();
