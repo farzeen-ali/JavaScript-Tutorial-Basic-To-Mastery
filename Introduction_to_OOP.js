65d71882cf61b6799a83c3190f57ac5c60621c23
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-12-03 22:43:04 +0500

    Add files via upload

---
+++ Introduction_to_OOP.js
@@ -0,0 +1,24 @@
+/******************** Object Oriented Programming (OOP) *********************
+One of the most common strategies in programming is to solve an issue by creating objects.
+
+DRY------> Don't Repeat Yourself
+
+************** Object *****************
+Anything in the world (living and non-living thing)
+An object is a member also known as instance of a class.
+Each Object has an:
+identity, behavior(actions), and a state(features / properties / attributes).
+
+************** Class *****************
+Blueprint to create objects.
+contains information to create a valid objects
+
+
+state of object ----> variables
+behavior of object ----> methods (functions)
+
+************** How To Model a Problem In OOPs **************
+Identity----> Class -----> Employee
+State-------> Attributes ----> empName, empAge, empSalary;
+Behavior----> Methods -----> getSalary();
+*/
