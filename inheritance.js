19a1ae68bf5d9082a77887efa98d1335c273bb71
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-12-07 21:47:06 +0500

    Add files via upload

---
+++ inheritance.js
@@ -0,0 +1,41 @@
+/*
+ ******************* Inheritance In JavaScript ***************
+ Inheritance enables you to define a class that takes all the functionality from a parent class and allows you to add more.
+
+Using class inheritance, a class can inherit all the methods and properties of another class.
+
+Inheritance is a useful feature that allows code reusability.
+
+To use class inheritance, you use the extends keyword.
+
+*/
+
+// parent / base / super class
+class Person{
+    constructor(name){
+        this.name = name;
+    }
+
+    greet(){
+        console.log(`hello I'm ${this.name}`);
+    }
+}
+
+//inherit 
+// child / derived / sub class
+class Student extends Person{
+   sayPresent(){
+    console.log("Present Sir");
+   }
+} 
+
+class Employee extends Person{
+   
+} 
+
+let student1 = new Student("Farzeen");
+student1.greet();
+student1.sayPresent();
+
+let employee1 = new Student("Ali");
+employee1.greet();
