12a42b5cf57d941f3c6792b97781b19677023d93
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-12-18 22:48:36 +0500

    Add files via upload

---
+++ JSON.js
@@ -0,0 +1,15 @@
+/* ************ JavaScript Object Notation **********************
+
+JSON --> JavaScript Object Notation
+*) It is a format for storing and transporting data
+*) Commonly used for API and config files
+*) Syntax: 
+     {
+        "name" : "Farzeen",
+        "age" : 22,
+        "marks" : [22,33,44,55,66]
+     }
+*) Language Independent
+*) Earlier XML was used instead of JSON
+*) JSON is faster, cleaner, light weight and easy to write and read than XML
+*/
